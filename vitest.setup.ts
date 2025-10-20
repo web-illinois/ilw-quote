a4444fa43c9b2322ce0413ca1027f28103d9e88f
Natalie Johnstone <nhj@illinois.edu>
2025-10-20 11:54:28 -0500

    updating colors

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,7 @@
 if (typeof document !== 'undefined') {
   const link = document.createElement('link');
   link.rel = 'stylesheet';
-  link.href = 'https://cdn.toolkit.illinois.edu/ilw-global/3/ilw-global.css';
+  link.href = 'https://dev.toolkit.illinois.edu/ilw-global/latest/ilw-global.css';
   document.head.appendChild(link);
 }
 
